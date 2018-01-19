6952e8a57c9f9240a2b0b944b01159521e4224b5
rtkcs <robino13@gmail.com>
2018-01-19 18:03:50 +0100

    switch, literals

---
+++ LanguageEnhancements/src/eu/rtakacs/literals/LiteralsTest.java
@@ -0,0 +1,14 @@
+package eu.rtakacs.literals;
+
+public class LiteralsTest {
+
+	public static void main(String[] args) {
+		
+		int x = 1___3;
+		long y = 1__________3;
+		float z = 3.141_592_653_59f;
+		System.out.println(x + " " + y + " " +z);
+
+	}
+
+}
--- LanguageEnhancements/src/eu/rtakacs/swtch/SwitchTest.java
+++ LanguageEnhancements/src/eu/rtakacs/swtch/SwitchTest.java
@@ -8,6 +8,10 @@ public class SwitchTest {
 
 	public static void main(String[] args) {
 		
+		//
+		//---SWITCH 1
+		//
+		System.out.println("-------switch 1");
 //		Set<String> literals = new TreeSet<String>();
 		Set<String> literals = new LinkedHashSet<String>();
 		literals.add("b");
@@ -27,6 +31,31 @@ public class SwitchTest {
 					System.out.println("b");
 					break;
 			}
+		}
+		
+		
+		//
+		//---SWITCH 2
+		//		
+		System.out.println("-------switch 2");
+//		String[] ss = new String[] {"a", "b", null}; //java.lang.NullPointerException
+		String[] ss = new String[] {"a", "b", "c"};
+		for(String s:ss) {
+			switch(s) {
+				case "a": System.out.println("a");
+//				case null : System.out.println("null"); //does not compile: case expressions must be constant expressions
+			}
+		}
+		
+		//
+		//---SWITCH 3
+		//
+		int val = 1_000_000;
+		switch(val) {
+			case 1_000_000: System.out.println("1_000_000");
+				break;
+			case 1000000: System.out.println("1_000_000");	//will not compile: Duplicate case
+				break;
 		}
 		
 	}
