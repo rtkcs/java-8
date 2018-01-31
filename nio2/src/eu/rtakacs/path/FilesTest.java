673e9fc68ffba59b1949cf3d5d1c2a53d52c2fea
rtkcs <robino13@gmail.com>
2018-01-31 10:45:07 +0100

    FilesTest

--- nio2/src/eu/rtakacs/path/FilesTest.java
+++ nio2/src/eu/rtakacs/path/FilesTest.java
@@ -1,5 +1,6 @@
 package eu.rtakacs.path;
 
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -9,6 +10,8 @@ import java.nio.file.LinkOption;
 import java.nio.file.OpenOption;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.attribute.BasicFileAttributeView;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.nio.file.attribute.PosixFilePermission;
 import java.util.Iterator;
 import java.util.LinkedList;
@@ -41,16 +44,26 @@ public class FilesTest {
 //		System.out.println("Files.lines(path, defaultCharset) = " + Files.lines(path, Charset.defaultCharset()));
 		
 		
-		System.out.println("Files.list : " );
+		Helper.print("Files.list : " );
 		Files.list(path).forEach(p -> System.out.println(" - " + p));
 		
+		Helper.print("Files.createTemp");
+		Path pathTempDir = Files.createTempDirectory("prefix");
+		System.out.println("Files.createTempDirectory = " + pathTempDir);
+		pathTempDir.toFile().deleteOnExit();
+		Path pathTempFile = Files.createTempFile("tempFilePrefix", ".suffix");
+		System.out.println("Files.createTempFile = " + pathTempFile);
+		pathTempFile.toFile().deleteOnExit();
+		
+		Path pathTempFile2 = Files.createTempFile(pathTempDir, "", ".temp");
+		System.out.println("Files.createTempFile(Path, prefix, suffix, FileAttribute<?>");
 		
 		
 //		System.out.println("Files. = " + Files.);
 //		System.out.println("Files. = " + Files.);
 //		System.out.println("Files. = " + Files.);
 		
-		System.out.print("Files.getPosixFilePermissions:");
+		Helper.print("Files.getPosixFilePermissions:");
 		try {
 			Set<PosixFilePermission> filePermissions = Files.getPosixFilePermissions(path);
 			Files.getPosixFilePermissions(path).forEach(s -> System.out.println(s.name()));
@@ -58,32 +71,86 @@ public class FilesTest {
 			System.out.println("  Exception = " + e.getClass());
 		}
 		
-		createFile();
+		Helper.print("Files.getFileAttributeView");
+		BasicFileAttributeView view = Files.getFileAttributeView(path, BasicFileAttributeView.class);
+		System.out.println("BasicFileAttributeView.name = " + view.name());
+		BasicFileAttributes attributes = view.readAttributes();
+		System.out.println("BasicFileAttributes.size = " + attributes.size());
+		System.out.println("BasicFileAttributes.isDirectory = " + attributes.isDirectory());
+		System.out.println("BasicFileAttributes.creationTime = " + attributes.creationTime());
+		System.out.println("BasicFileAttributes.isSymbolicLink = " + attributes.isSymbolicLink());
+		System.out.println("BasicFileAttributes.isRegularFile = " + attributes.isRegularFile());
+		System.out.println("BasicFileAttributes.fileKey = " + attributes.fileKey());
+		System.out.println("BasicFileAttributes.isOther = " + attributes.isOther());
+		System.out.println("BasicFileAttributes.lastAccessTime = " + attributes.lastAccessTime());
+		System.out.println("BasicFileAttributes.lastModifiedTime = " + attributes.lastModifiedTime());
+		
+		
+		filesWrite1();
+		filesWrite2();
+		filesWrite3();
 	}
 	
-	public static void createFile() {
-		
-		Path pathResources = Paths.get("","resources").toAbsolutePath();
-		System.out.println(pathResources);
-//		Path path = Paths.get("", "testFile.txt");
-		Path pathTestFile = pathResources.resolve("testFile.txt");
-		System.out.println(pathTestFile);
+	public static void filesWrite1() {
+		Helper.print("filesWrite1");
+		Path pathResources = Paths.get("","resources", "testFile1.txt").toAbsolutePath();
+		System.out.println("Creating file: " + pathResources);
+
 		try {
-			Files.deleteIfExists(pathTestFile);
+			Files.deleteIfExists(pathResources);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
 		List<String> content = new LinkedList<>();
+		content.add("File written with function Files.write(Path, Iterable<? extends CharSequence>, Charset, OpenOptions ...");
 		content.add("1. line");
 		content.add("2. line");
 		content.add("3. line");
 		
 		try {
-			Files.write(pathTestFile, content, Charset.forName("utf-8"));
+			Files.write(pathResources, content, Charset.forName("utf-8"));
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public static void filesWrite2() {
+		Helper.print("filesWrite2");
+		Path pathResources = Paths.get("","resources", "testFile2.txt").toAbsolutePath();
+		System.out.println("Creating file: " + pathResources);
+		
+		StringBuilder sb = new StringBuilder("File written with function Files.write(Path, bytes[])");
+		sb.append(System.lineSeparator());
+		sb.append("1. line");
+		sb.append(System.lineSeparator());
+		sb.append("2. line");
+		sb.append(System.lineSeparator());
+		sb.append("3. line");
+		
+		try {
+			Files.write(pathResources, sb.toString().getBytes());
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void filesWrite3() {
+		Helper.print("filesWrite3");
+		Path pathResources = Paths.get("","resources", "testFile3.txt").toAbsolutePath();
+		System.out.println("Creating file: " + pathResources);
+		
+		try (BufferedWriter bw = Files.newBufferedWriter(pathResources, Charset.forName("utf-8"))){
+			bw.write("File written with BufferedWriter, Files.newBufferedWriter(Path path, Charset cs, OpenOption... options)");
+			bw.newLine();
+			bw.write("1. line");
+			bw.newLine();
+			bw.write("2. line");
+			bw.newLine();
+			bw.write("3. line");
+			
+		}catch(IOException e) {
 			e.printStackTrace();
 		}
 	}
