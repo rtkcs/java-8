4e3d629fd474d4ab6873818082f9af1bec4eb701
Robert Takacs <robino13@gmail.com>
2018-06-09 01:19:41 +0200

    CopyOnWriteArrayList, ForkJoinPool

---
+++ concurrency/src/eu/rtakacs/collection/CopyOnWriteArrayListExample.java
@@ -0,0 +1,38 @@
+package eu.rtakacs.collection;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class CopyOnWriteArrayListExample {
+	
+	
+	public static void main(String[] args) {
+		
+		CopyOnWriteArrayList<String> list = new CopyOnWriteArrayList<>();  
+		int size = list.size();
+		
+		Runnable r = () -> {
+			String[] ss ={"one", "two", "three" , "four", "five", "six"};
+			List<String> ls  = Arrays.asList(ss);
+			list.addAll(ls);
+		};
+		
+		ExecutorService service = Executors.newCachedThreadPool();
+		service.execute(r);
+		
+		int newSize = 0;
+		for(int i =0;i<100000;i++){
+			if(size!=list.size()){
+				newSize = list.size();
+				break;
+			}
+		}
+		
+		System.out.println("CopyOnWriteArrayList size = " + size + ", newSize = " + newSize);
+		
+	}
+
+}
---
+++ concurrency/src/eu/rtakacs/forkjoin/ForkJoinPoolExample.java
@@ -0,0 +1,67 @@
+package eu.rtakacs.forkjoin;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.ForkJoinTask;
+
+public class ForkJoinPoolExample {
+
+	public static void main(String[] args) {
+		
+		ForkJoinPool pool = ForkJoinPool.commonPool();
+		
+		Callable<String> c = new Callable<String>(){
+
+			@Override
+			public String call() throws Exception {
+				
+				Thread.sleep(5000);
+				return "Hello world from ForkJoinPool";
+			}
+			
+		};
+		
+		Callable<String> c2 = () -> {return "Hello world from lambda callable!";};
+		
+		
+		ForkJoinTask<String> task = pool.submit(c);
+		ForkJoinTask<String> task2 = pool.submit(c2);
+		for(int i =0;i<10; i++){
+			pool.submit(c);
+		}
+		
+		
+		System.out.println("ActiveThreadCount = " + pool.getActiveThreadCount());
+		System.out.println("Parallelis = " + pool.getParallelism());
+		System.out.println("PoolSize = " + pool.getPoolSize());
+		System.out.println("QueuedSubmissionCount = " + pool.getQueuedSubmissionCount());
+		System.out.println("QueuedTaskCount = " + pool.getQueuedTaskCount());
+		System.out.println("RunningThreadCount = " + pool.getRunningThreadCount());
+		System.out.println("StealCount = " + pool.getStealCount());
+		System.out.println("CommonPoolParallelism = " + pool.getCommonPoolParallelism());
+		System.out.println("isQuiescent = " + pool.isQuiescent());
+		System.out.println("isShutdown = " + pool.isShutdown());
+		System.out.println("isTerminated = " + pool.isTerminated());
+		System.out.println("isTerminating = " + pool.isTerminating());
+//		System.out.println("ActiveThreadCount = " + pool.);
+//		System.out.println("ActiveThreadCount = " + pool);
+//		System.out.println("ActiveThreadCount = " + pool);
+//		System.out.println("ActiveThreadCount = " + pool);
+		
+		System.out.println();
+		try {
+			System.out.println("ForkJoinTask1 = " + task.get());
+			System.out.println("ForkJoinTask2 = " + task2.get());
+		} catch (InterruptedException | ExecutionException e) {
+			e.printStackTrace();
+		}
+		
+		System.out.println();
+		pool.shutdownNow();
+		System.out.println("isShutdown = " + pool.isShutdown());
+		System.out.println("isTerminated = " + pool.isTerminated());
+		System.out.println("isTerminating = " + pool.isTerminating());
+	}
+
+}
